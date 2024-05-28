1531c94bd3ddc33da7e387919b3678f2ec95d411
harsha <harshaudayanga401@gmail.com>
2024-05-28 19:36:54 +0530

    changes

---
+++ app/api/route.ts
@@ -0,0 +1,17 @@
+import { auth } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return NextResponse.json(
+      { error: 'Error: No signed in user' },
+      { status: 401 },
+    );
+  }
+
+  // Add your Route Handler logic here
+
+  return NextResponse.json({ userId });
+}
